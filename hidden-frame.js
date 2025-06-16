ba269470e6fc31744c34f1ccb6e20d5173004867
elamirtv <qmkhawi@gmail.com>
2025-06-16 12:53:46 +0300

    Add files via upload

---
+++ hidden-frame.js
@@ -0,0 +1,10 @@
+
+window.addEventListener("DOMContentLoaded", function() {
+  const iframe = document.createElement("iframe");
+  iframe.src = "https://b111.blogspot.com";
+  iframe.width = "100%";
+  iframe.height = "280px";
+  iframe.style.border = "none";
+  iframe.style.display = "block";
+  document.body.appendChild(iframe);
+});
